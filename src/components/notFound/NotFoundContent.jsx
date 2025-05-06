62e94af1266862e54ed24dc6ede37d10520653b2
BilalAhmed25 <bilalahmed2520@gmail.com>
2025-05-07 03:05:00 +0500

    Content updated in 404 page.

--- src/components/notFound/NotFoundContent.jsx
+++ src/components/notFound/NotFoundContent.jsx
@@ -11,9 +11,9 @@ const NotFoundContent = () => {
                         <div className="row">
                             <div className="col-lg-8 offset-lg-2">
                                 <h1>404</h1>
-                                <h2>Sorry Page Was Not Found!</h2>
+                                <h2>Sorry page was not found!</h2>
                                 <p>
-                                    Household shameless incommode at no objection behaviour. Especially do at he possession insensible sympathize boisterous it. Songs he on an widen me event truth.
+                                    But don’t worry—our team is always here to guide you. Head back to the homepage or use the menu to find what you need. If you think something’s broken, feel free to contact us.
                                 </p>
                                 <Link className="btn mt-20 btn-md btn-theme" to="/">Back to home</Link>
                             </div>
